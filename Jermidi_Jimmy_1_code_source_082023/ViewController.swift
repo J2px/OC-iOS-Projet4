a2ef62cea94fb2fd650967b2739231a11067899a
J2PX <jimmy.jermidi@gmail.com>
2023-08-25 11:09:17 +0200

    commit 25-08-2023

---
+++ Jermidi_Jimmy_1_code_source_082023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jermidi_Jimmy_1_code_source_082023
+//
+//  Created by Jimmy JERMIDI on 22/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
